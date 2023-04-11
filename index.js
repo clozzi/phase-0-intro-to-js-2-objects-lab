f1a1d795a6cbf41577ce505eb73154950abc089f
Christopher Lozzi <clozzi12@gmail.com>
2023-04-11 13:19:06 -0400

    objects lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution in this file!
+const employee = {
+    name : `Bob`,
+    streetAddress : `123 ABC Ave`,
+}
+
+function updateEmployeeWithKeyAndValue() {
+  let newEmployee = {...employee, name: `Sam`, streetAddress: `11 Broadway`}
+    return newEmployee
+}
+
+beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress) {
+    employee[streetAddress] = "12 Broadway"
+    return employee
+  }
+
+  function deleteFromEmployeeByKey() {
+    let newEmployee = {...employee, name: undefined}
+      return newEmployee
+  }
+
+  beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+
+  function destructivelyDeleteFromEmployeeByKey(employee,name) {
+    employee[name] = undefined
+    return employee
+  }
